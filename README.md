7ba8917d2305ae2d99a4ab5abcaf38f8c6641510
RENZO DAMIAN FERREIRA DÍAZ <55892169@ceibal.edu.uy>
2024-09-09 18:34:23 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PreeEntrega2
---
+++ src/routes/productsRoutes.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import fs from 'fs';
+
+const router = Router()
+
+router.get('/',async(req,res)=>{
+    const data = await fs.promises.readFile('products.json','utf-8');
+    const products = JSON.parse(data)
+    res.status(200).json(products)
+})
+
+router.get('/:id',async (req,res)=>{
+    const {id} = req.params
+    const data = await fs.promises.readFile('products.json','utf-8')
+    const products = JSON.parse(data)
+    const product = products.find(product => product.id === id)
+
+
+    res.status(200).json(product)
+})
+
+
+router.post('/',async (req,res)=>{
+    const body = req.body
+    const data = await fs.promises.readFile('products.json','utf-8')
+    const products = JSON.parse(data)
+    /*if(products.some(product=> product.code === body.code)){
+        return res.status(200).json({error: "Producto ya existente"})
+    }
+    const newProduct = {
+        
+        id:crypto.randomUUID(),
+        ...body
+
+    }
+    products.push(newProduct)
+
+     await fs.promises.writeFile('products.json',JSON.stringify(products))*/
+    res.status(200).json({messge:"Producto creado exitosamente."})
+})
+
+
+
+
+export default router
